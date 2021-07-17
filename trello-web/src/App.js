2367af0b1ad427fa546c185f207e7244c17acf91
thanghn <ngocthanguit@gmail.com>
2021-07-17 11:23:57 +0700

    design p1

--- trello-web/src/App.js
+++ trello-web/src/App.js
@@ -1,7 +1,129 @@
+import './style.scss'
 
 function App() {
   return (
-    <h1>HELLO 1</h1>
+    <div className="trello-master">
+      <div className="navbar app">App Bar</div>
+      <div className="navbar board">
+        Board Bar
+      </div>
+      <div className="board-columns">
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://static.remove.bg/remove-bg-web/8be32deab801c5299982a503e82b025fee233bd0/assets/start-0e837dcc57769db2306d8d659f53555feb500b3c5d456879b9c843d1872e7baa.jpg" alt="logo" />
+                {/* Title: THANGHN DEV */}
+            </li>
+            <li>Lorem ipsum dolor sit amet</li>
+            <li>Lorem ipsum dolor sit amet</li>
+            <li>Lorem ipsum dolor sit amet</li>
+            <li>Lorem ipsum dolor sit amet</li>
+            <li>Lorem ipsum dolor sit amet</li>
+            <li>Lorem ipsum dolor sit amet</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://static.remove.bg/remove-bg-web/8be32deab801c5299982a503e82b025fee233bd0/assets/start-0e837dcc57769db2306d8d659f53555feb500b3c5d456879b9c843d1872e7baa.jpg" alt="logo" />
+              {/* Title: THANGHN DEV */}
+            </li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://static.remove.bg/remove-bg-web/8be32deab801c5299982a503e82b025fee233bd0/assets/start-0e837dcc57769db2306d8d659f53555feb500b3c5d456879b9c843d1872e7baa.jpg" alt="logo" />
+              {/* Title: THANGHN DEV */}
+            </li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://static.remove.bg/remove-bg-web/8be32deab801c5299982a503e82b025fee233bd0/assets/start-0e837dcc57769db2306d8d659f53555feb500b3c5d456879b9c843d1872e7baa.jpg" alt="logo" />
+              {/* Title: THANGHN DEV */}
+            </li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://static.remove.bg/remove-bg-web/8be32deab801c5299982a503e82b025fee233bd0/assets/start-0e837dcc57769db2306d8d659f53555feb500b3c5d456879b9c843d1872e7baa.jpg" alt="logo" />
+              {/* Title: THANGHN DEV */}
+            </li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://static.remove.bg/remove-bg-web/8be32deab801c5299982a503e82b025fee233bd0/assets/start-0e837dcc57769db2306d8d659f53555feb500b3c5d456879b9c843d1872e7baa.jpg" alt="logo" />
+              {/* Title: THANGHN DEV */}
+            </li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://static.remove.bg/remove-bg-web/8be32deab801c5299982a503e82b025fee233bd0/assets/start-0e837dcc57769db2306d8d659f53555feb500b3c5d456879b9c843d1872e7baa.jpg" alt="logo" />
+              {/* Title: THANGHN DEV */}
+            </li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem</li>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+								
+      </div>
+    </div>
   );
 }
 
